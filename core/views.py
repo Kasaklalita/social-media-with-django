720b715c245c271784c8a06ded5f38d1d1509025
Veniamin Polienko <kasaklalita@mail.ru>
2023-01-28 17:09:55 +0300

    Added signup page

--- core/views.py
+++ core/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Profile
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'This email is already taken.')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'This username is already taken.')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
+                user.save()
+                # Log user in and redirect to the settings page
+
+                # Create a Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(
+                    user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
+        else:
+            messages.info(request, 'The passwords don`t match.')
+            return redirect('signup')
+    else:
+        pass
+    return render(request, 'signup.html')
